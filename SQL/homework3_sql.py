6718b05c9046d611fa2b9f50d62f291236fb0c50
Sergey Chiiachenko <chiachenkosergey@gmail.com>
2022-10-27 19:23:04 +0300

    Filled the rows of the person table with data

--- SQL/homework3_sql.py
+++ SQL/homework3_sql.py
@@ -1,6 +1,9 @@
 import sqlite3
-import names
 from faker import Faker
+
+
+faker = Faker(['en_US'])
+
 
 def create_person_tb():
     db = 'homework3.db'
@@ -18,4 +21,27 @@ def create_person_tb():
     cur.execute(sql)
     con.close()
 
+
+def ins_data():
+    db = 'homework3.db'
+    con = sqlite3.connect(db)
+    cur = con.cursor()
+    for i in range(10):
+        f_name = faker.first_name()
+        l_name = faker.last_name()
+        addr = faker.address()
+        job = faker.job()
+        age = faker.random_int(min=0, max=100)
+        str_data = (f_name, l_name, addr, job, age)
+        sql = f'''
+        INSERT INTO person (first_name, last_name, address, job, age)
+        VALUES
+        {str_data}
+        '''
+        cur.execute(sql)
+        con.commit()
+    con.close()
+
+
 create_person_tb()
+ins_data()
